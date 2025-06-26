a3f2ea0387783e5907d172a3c4a1c513e6127abe
mar-azm <maria.a.sulej@gmail.com>
2025-06-26 14:58:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
-import csv  # do ładowania plików csv
-import yaml  # do ładowania plików yaml
-import random  # do losowania liczb (przy numerze badanego)
-import atexit  # do pobierania danych z procedury do pliku
-import codecs  # do pobrania polskich znaków
-from typing import List, Dict # do używania listy i słownika
-from os.path import join # do łączenia plików i danych
-from psychopy import visual, event, logging, gui, core # do ładowania potrzebnych rzeczy z psychopy
+import csv  # Do ładowania plików csv.
+import yaml  # Do ładowania plików yaml.
+import random  # Do losowania liczb (przy numerze badanego).
+import atexit  # Do pobierania danych z procedury do pliku.
+import codecs  # Do pobrania polskich znaków.
+from typing import List, Dict # Do używania listy i słownika.
+from os.path import join # Do łączenia plików i danych.
+from psychopy import visual, event, logging, gui, core # Do ładowania potrzebnych rzeczy z psychopy.
 
 
 @atexit.register
 
 # Dzięki atexit ta funkcja zawsze zostanie wykonana:
-# wyniki badanego będą z awsze zapisane, nawet jak coś przerwie działanie kodu.
+# Wyniki badanego będą z awsze zapisane, nawet jak coś przerwie działanie kodu.
 
 def save_beh_results() -> None:
    """
    Tworzenie w pliku results notatkę do zapisania danych badanego.
    """
-   file_name = PART_ID + '_' + str(random.choice(range(100, 1000))) + '_beh.csv'
-   with open(join('results', file_name), 'w', encoding='utf-8') as beh_file:
+   file_name = PART_ID + '_' + str(random.choice(range(100, 1000))) + '_beh.csv' # Nadawanie nazwy notatce z wynikami badanego.
+   with open(join('results', file_name), 'w', encoding='utf-8') as beh_file: # Zaspisywanie danych w pliku.
        beh_writer = csv.writer(beh_file)
        beh_writer.writerows(RESULTS)
    logging.flush()
@@ -99,8 +99,9 @@ def abort_with_error(err: str) -> None:
     raise SystemExit(err)
 
 
-def run_trial(win, conf, clock, target_stim, instruction_stim, fix_cross, reminder_stim, previous_instruction, no_switch_count, training=False):
+def run_trial(win, conf, clock, target_stim, cue_stim, fix_cross, reminder_stim, previous_cue, no_switch_count, training=False):
     """
+    Przebieg pojedynczej sekwencji
     Prepare and present single trial of procedure.
     Input (params) should consist all data need for presenting stimuli.
     If some stimulus (eg. text, label, button) will be presented across many trials.
@@ -109,87 +110,83 @@ def run_trial(win, conf, clock, target_stim, instruction_stim, fix_cross, remind
         All behavioral data (reaction time, answer, etc. should be returned from this function).
     """
 
-    if previous_instruction is None:
-        instruction = random.choice(conf['STIM_CUE'])
+    if previous_cue is None: # Losowanie pierwszej wskazówki - LITERA lub CYFRA.
+        cue = random.choice(conf['STIM_CUE'])
         no_switch_count = 0
     else:
-        if no_switch_count >= 5:
-            instruction = "CYFRA" if previous_instruction == "LITERA" else "LITERA"
+        if no_switch_count >= 5: # Zmiana wskazówki, jeśli nie było zmiany przez ostatnie 5 sekwencji.
+            cue = "CYFRA" if previous_cue == "LITERA" else "LITERA"
             no_switch_count = 0
         else:
-            if random.random() < 0.25:
-                instruction = "CYFRA" if previous_instruction == "LITERA" else "LITERA"
+            if random.random() < 0.25: # Prawdopodobieństwo zmienienia wskazówki.
+                cue = "CYFRA" if previous_cue == "LITERA" else "LITERA"
                 no_switch_count = 0
             else:
-                instruction = previous_instruction
-                no_switch_count += 1
+                cue = previous_cue
+                no_switch_count += 1 # Liczenie ile razy powtarzała się dana wskazówka.
 
-   if instruction == previous_instruction or previous_instruction == None:
+    if cue == previous_cue or previous_cue == None: # Zebranie informacji czy zadanie było "switch", czy "no-switch".
         switch_status = "no-switch"
-   else:
+    else:
         switch_status = "switch"
 
-    litera = random.choice(conf['STIM_LETTERS'])
-    cyfra = random.choice(conf['STIM_NUMBERS'])
-    instruction_stim.text = instruction
+    litera = random.choice(conf['STIM_LETTERS']) # Wybranie ze zbioru liter losowej litery do bodźca docelowego.
+    cyfra = random.choice(conf['STIM_NUMBERS']) # Wybranie ze zbioru cyfr losowej cyfry do bodźca docelowego.
+    cue_stim.text = cue # Przechowywanie tekstu wskazówki.
 
-    for _ in range(conf['STIM_TIME']):
-        check_exit() # sprawdzenie F7
-        instruction_stim.draw()
+    for _ in range(conf['STIM_TIME']): # Wyświetlenie wskazówki.
+        check_exit() # Sprawdzenie, czy nie został wciśnięty klawisz F7.
+        cue_stim.draw()
         reminder_stim.draw()
         win.flip()
 
-    for _ in range(conf['FIX_CROSS_TIME']):
-        check_exit() # sprawdzenie F7
+    for _ in range(conf['FIX_CROSS_TIME']): # Wyświetlenie punktu fiksacji.
+        check_exit() # Sprawdzenie, czy nie został wciśnięty klawisz F7.
         fix_cross.draw()
         reminder_stim.draw()
         win.flip()
 
-    # === Start trial ===
-    # This part is time-crucial. All stims must be already prepared.
-    # Only .draw() .flip() and reaction related stuff goes there.
-    target_stim.text = f"{litera} {cyfra}"
-    event.clearEvents()
-    # make sure, that clock will be reset exactly when stimuli will be drawn
-    win.callOnFlip(clock.reset)
+    target_stim.text = f"{litera} {cyfra}" # Przechowywanie bodźców docelowych w formie tekstu.
+    event.clearEvents() # Zresetowanie każdego wcześniejszego wciśnięcia klawiszu.
+    win.callOnFlip(clock.reset) # Dokładne rozpoczęcie czasu pomiaru od momentu wyświetlenia bodźca.
 
-    for _ in range(conf['REACTION_TIME']):  # present stimuli
-        check_exit() # sprawdzenie F7
-        reaction = event.getKeys(keyList=list(conf['REACTION_KEYS']), timeStamped=clock)
-        if reaction:  # break if any button was pressed
+    for _ in range(conf['REACTION_TIME']):  # Wyświetlenie bodźca docelowego.
+        check_exit() # Sprawdzenie, czy nie został wciśnięty klawisz F7.
+        reaction = event.getKeys(keyList=list(conf['REACTION_KEYS']), timeStamped=clock) # Zarejestrowanie wciśnięcia klawisza i przypisanie czasu reakcji.
+        if reaction:  # Zatrzymanie po wciśnięciu klawisza.
             break
-        target_stim.draw()
-        reminder_stim.draw()
+        target_stim.draw() # Wyświetlanie bodźca docelowego.
+        reminder_stim.draw() # Wyświetlanie przypomnienia o przypisaniu klawiszy do bodźców.
         win.flip()
 
-    if not training: # maska w sesji eksperymentalnej pojawia się po pokazaniu bodźców, zaś w treningu po feedbacku
+    if not training: # Maska w sesji eksperymentalnej pojawia się po pokazaniu bodźców, zaś w treningu po feedbacku.
         for _ in range(conf['MASK_TIME']):
-            check_exit()
+            check_exit() # Sprawdzenie, czy nie został wciśnięty klawisz F7.
             reminder_stim.draw()
             win.flip()
 
     if reaction:
-        key_pressed, rt = reaction[0]  # 0 to krotka: (pierwszy przycisk który został wciśnięty, czas wciśnięcia)
-    else:  # timeout
+        key_pressed, rt = reaction[0]  # 0 to krotka: (pierwszy przycisk który został wciśnięty, czas wciśnięcia).
+    else:  # Po upłynięciu czasu na reakcję na bodziec zapisujemy czas reakcji jako -1, aby wyróżniał się z rzeczywistych czasów reakcji.
         key_pressed = 'no_key'
-        rt = -1.0  # co z tym czy powinno w pliku wynikowym byc jako timeout?
+        rt = -1
 
-    correct_key = None  # wprowadzenie zmiennej (domyślnie żadna zmienna)
-    if instruction == "LITERA":
+    correct_key = None  # Wprowadzenie prawidłowej reakcji (klawiszu) na dane bodźce (domyślnie żaden klawisz).
+    if cue == "LITERA": # Jeśli wskazówką była LITERA, poprawnym klawiszem w odpowiedzi na samogłoskę jest "z", a na spółgłoskę "m".
         correct_key = 'z' if litera in ['A', 'E', 'I', 'U'] else 'm'
-    elif instruction == "CYFRA":
+    elif cue == "CYFRA": # Jeśli wskazówką była CYFRA, poprawnym klawiszem w odpowiedzi na cyfry nieparzyste jest "z", a na cyfry parzyste "m".
         correct_key = 'z' if cyfra in ['3', '5', '7', '9'] else 'm'
 
-    correctness = 1 if key_pressed == correct_key else 0
+    correctness = 1 if key_pressed == correct_key else 0 #Poprawna odpowiedź zapisana jako 1, a niepoprawna jako 0.
 
-    return key_pressed, rt, switch_status, correctness, instruction  # return all data collected during trial
+    return key_pressed, rt, switch_status, correctness, cue  # Zwracanie wszystkich danych zebranych podczas sekwencji.
 
 
 
 # GLOBAL VARIABLES
 
 RESULTS = list()  # list in which data will be collected
-RESULTS.append(['PART_ID', 'Block', 'Trial', 'Instruction', 'Correctness', 'Switch_status', 'RT'])  # Results header
+RESULTS.append(['PART_ID', 'Block', 'Trial', 'Cue', 'Correctness', 'Switch_status', 'RT'])  # Results header
 PART_ID = ''
 SCREEN_RES = []
 
@@ -218,7 +215,7 @@ logging.info('SCREEN RES: {}'.format(SCREEN_RES))
 # Stworzenia bodźców
 
 fix_cross = visual.TextStim(win, text='+', height=100, color=conf['FIX_CROSS_COLOR'])
-instruction_stim = visual.TextStim(win, text="", height=conf['STIM_SIZE'], color=conf['STIM_COLOR'])
+cue_stim = visual.TextStim(win, text="", height=conf['STIM_SIZE'], color=conf['STIM_COLOR'])
 target_stim = visual.TextStim(win, text="", height=conf['STIM_SIZE'], color=conf['STIM_COLOR'])
 reminder_text = read_text_from_file(join('.', 'messages', 'przypomnienie.txt'))
 reminder_stim = visual.TextStim(win, text=reminder_text, pos=(0, -SCREEN_RES[1] // 2 + 100), height=20, color='black', wrapWidth=SCREEN_RES[0] - 100)
@@ -229,13 +226,13 @@ reminder_stim = visual.TextStim(win, text=reminder_text, pos=(0, -SCREEN_RES[1] 
 show_info(win, join('.', 'messages', 'instrukcja.txt'))
 show_info(win, join('.', 'messages', 'komunikattrening.txt'))
 show_info(win, join('.', 'messages', 'start.txt'))
-previous_instruction = None
+previous_cue = None
 no_switch_count = 0
 for trial_no in range(conf['TRAINING_TRIALS']):
-    key_pressed, rt, switch_status, correctness, instruction = run_trial(win, conf, clock, target_stim, instruction_stim, fix_cross, reminder_stim, previous_instruction, no_switch_count, training=True)
-    previous_instruction = instruction
+    key_pressed, rt, switch_status, correctness, cue = run_trial(win, conf, clock, target_stim, cue_stim, fix_cross, reminder_stim, previous_cue, no_switch_count, training=True)
+    previous_cue = cue
     corr = correctness
-    RESULTS.append([PART_ID, 'training', trial_no, instruction, corr, switch_status, rt])
+    RESULTS.append([PART_ID, 'training', trial_no, cue, corr, switch_status, rt])
     feedb = "Poprawnie" if corr else "Niepoprawnie"
     feedb = visual.TextStim(win, text=feedb, height=50, color=conf['STIM_COLOR'])
     for _ in range(int(frame_rate * 1)):  # 1 sekunda (zamiana core.wait(1) na pętlę)
@@ -255,9 +252,9 @@ show_info(win, join('.', 'messages', 'start.txt'))
 trial_no = 0
 for block_no in range(conf['NO_BLOCKS']):
     for _ in range(conf['TRIALS_IN_BLOCK']):
-        key_pressed, rt, switch_status, corr, instruction = run_trial(win, conf, clock, target_stim, instruction_stim, fix_cross, reminder_stim, previous_instruction, no_switch_count, training=False)
-        previous_instruction = "LITERA"
-        RESULTS.append([PART_ID, block_no, trial_no, instruction, corr, switch_status, rt])
+        key_pressed, rt, switch_status, corr, cue = run_trial(win, conf, clock, target_stim, cue_stim, fix_cross, reminder_stim, previous_cue, no_switch_count, training=False)
+        previous_cue = "LITERA"
+        RESULTS.append([PART_ID, block_no, trial_no, cue, corr, switch_status, rt])
         trial_no += 1
         for _ in range(int(frame_rate * 1)):  # przypomnienie jest widoczne przez 1s między próbami, aby znikało i nie rozpraszało badanego
             check_exit()
